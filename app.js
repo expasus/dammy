1b9b7ce81ee59e387fa25cb3c276bc781f039dbe
AdamMahdad <136065266+AdamMahdad@users.noreply.github.com>
2024-03-08 06:21:33 +0100

    e

--- app.js
+++ app.js
@@ -91,3 +91,13 @@ Info.onclick = function(){
 }
 
 });
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar")
+  sidebar.style.display = "flex"
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar")
+  sidebar.style.display = "none"
+}
